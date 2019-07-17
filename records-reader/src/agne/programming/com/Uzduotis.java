2fc9ed27e5d938fbc6eb9b6c0927312d6d6b9f58
Agne <agne@lukoseviciute.com>
2019-07-17 23:28:09 +0300

    adding gson jar file

--- records-reader/src/agne/programming/com/Uzduotis.java
+++ records-reader/src/agne/programming/com/Uzduotis.java
@@ -1,6 +1,5 @@
 package agne.programming.com;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,21 +10,23 @@ public class Uzduotis {
 
 		String csvFile = "/Users/user/Documents/records.csv";
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))){
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+			
 			//skips first line "Athlete, Mark"
 			String currLine = reader.readLine();
-			
 
 			while ((currLine = reader.readLine()) != null){
 
 				String[] recordInfo = currLine.split(",");
 				System.out.println(recordInfo[2] + ", " + recordInfo[1]);
 			}
-
+			reader.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }

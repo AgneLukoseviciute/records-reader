c15c402f86f90c0ee93cd76d4e57880e7c192c60
Agne <agne@lukoseviciute.com>
2019-07-18 21:11:44 +0300

    putting csv into assets

--- records-reader/src/agne/programming/com/Uzduotis.java
+++ records-reader/src/agne/programming/com/Uzduotis.java
@@ -1,23 +1,23 @@
 package agne.programming.com;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 
 public class Uzduotis {
 
 	public static void main(String[] args) {
 		
-		Gson gson;
+		//Gson gson;
 		
-		
-		//String csvFile = "/Users/user/Documents/records.csv";
 
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader("records.csv"));
 			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					Thread.currentThread().getContextClassLoader().getResourceAsStream("records.csv")));
+					
 			//skips first line "Athlete, Mark"
 			String currLine = reader.readLine();
 

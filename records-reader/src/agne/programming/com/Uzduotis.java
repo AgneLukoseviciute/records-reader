17a13ad9b59672d475f63c1bde50ce3a0d9c3523
Agne <agne@lukoseviciute.com>
2019-07-20 18:40:44 +0300

    refactoring, simplifying main

--- records-reader/src/agne/programming/com/Uzduotis.java
+++ records-reader/src/agne/programming/com/Uzduotis.java
@@ -5,54 +5,20 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Arrays;
-
 import com.google.gson.Gson;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-
 public class Uzduotis {
 
 	public static void main(String[] args) {
-
-		Gson gson = new Gson();
-		File jsonFile = new File ("assets/records_tweaked.json");
-		File csvFile = new File ("assets/records.csv");
+		CSVFileReader csvRead = new CSVFileReader();
+		csvRead.printInfo();
 		
-		try {
-	
-			
-			Reader jsonReader = new FileReader(jsonFile);
-			
-			//create Athlete objects from JSON
-			Athlete[] athletes = gson.fromJson(jsonReader, Athlete[].class);
-			
-			for (int i =0; i< athletes.length; i++) {
-				System.out.println(athletes[i].getName());
-			}
-			
-			//System.out.println(athlete);
-			
-			BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
-
-			//skips first line "Athlete, Mark"
-			String currLine = csvReader.readLine();
-
-			while ((currLine = csvReader.readLine()) != null){
-
-				String[] recordInfo = currLine.split(",");
-				System.out.println(recordInfo[2] + ", " + recordInfo[1]);
-			}
-			csvReader.close();
-			jsonReader.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		JSONFileReader jsonRead = new JSONFileReader();
+		jsonRead.printInfo();
 	}
-
+	
 }
 
 class Athlete {
@@ -72,14 +38,6 @@ class Athlete {
 	@SerializedName("Location")
 	@Expose
 	private String location;
-
-	/*public Athlete(Integer rank, String mark, String name, String date, String location) {
-		this.rank = rank;
-		this.mark = mark;
-		this.name = name;
-		this.date = date;
-		this.location = location; 
-	}*/
 
 	public Integer getRank() {
 		return rank;
@@ -110,11 +68,59 @@ abstract class MyFileReader{
 
 class CSVFileReader extends MyFileReader{
 	
+	public CSVFileReader () {
+		
+	}
+	
+	public void printInfo() {
+		File csvFile = new File ("assets/records.csv");
+		BufferedReader csvReader;
+		try {
+			csvReader = new BufferedReader(new FileReader(csvFile));
+			
+			//skips first line "Athlete, Mark"
+			String currLine = csvReader.readLine();
+
+			while ((currLine = csvReader.readLine()) != null){
+
+				String[] recordInfo = currLine.split(",");
+				System.out.println(recordInfo[2] + ", " + recordInfo[1]);
+			}
+			
+			csvReader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
 
 class JSONFileReader extends MyFileReader {
 	
+	public JSONFileReader() {
+		
+	}
 	
+	public void printInfo() {
+		Gson gson = new Gson();
+		File jsonFile = new File ("assets/records_tweaked.json");
+		Reader jsonReader;
+		
+		try {
+			jsonReader = new FileReader(jsonFile);
+			
+			//creates an array of athlete objects storing JSON data
+			Athlete[] athletes = gson.fromJson(jsonReader, Athlete[].class);
+			
+			for (int i =0; i< athletes.length; i++) {
+				System.out.println(athletes[i].getName());
+			}
+			jsonReader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
